9e2d59ad4a2e04dcf5a9c807861a6cc6d40b9603
pepetgoodboy <iqbaltw127@gmail.com>
2024-06-22 08:27:04 +0700

    fix route

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
+/* eslint-disable no-unused-vars */
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
