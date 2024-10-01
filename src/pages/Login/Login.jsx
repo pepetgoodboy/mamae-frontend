f19dfa9015a19e7ccb282d3e2545b3df18f9e0cc
Pepet Goodboy <123438521+pepetgoodboy@users.noreply.github.com>
2024-10-01 16:37:03 +0700

    Update Login.jsx
    change api url

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -20,7 +20,7 @@ const Login = () => {
     setLoading(true);
     try {
       const response = await axios.post(
-        "https://automatic-jewelle-mamae-laundry-df687c23.koyeb.app/api/user/login",
+        "https://yappy-sadella-curhatin-57043035.koyeb.app//api/user/login",
         {
           email,
           password,
